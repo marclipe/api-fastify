301ea43342524e613354136acc1b492aebbe0a1c
MarcLipe <marcos.lima52@fatec.sp.gov.br>
2023-08-25 19:14:36 -0300

    Perfoming queries with knex

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import fastify from 'fastify'
+import crypto from 'node:crypto'
 import { knex } from './database'
 
 const app = fastify()
 
 // http://localhost:3333/hello
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
 
-  return tables
+  return transaction
 })
 
 app
